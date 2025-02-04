6dad820e1b9e217e12365bac9da0372f26108ff2
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2025-02-04 05:36:31 -0300

    Varios cambios
    No se mas Tokenizer, se usa TextVectorization.
    A partir de eso se mejoro el training y la ai.

--- liz/ai.py
+++ liz/ai.py
@@ -1,6 +1,6 @@
 import os
-import tensorflow as tf
 import numpy as np
+import keras
 import pickle
 import pyaudio
 import vosk
@@ -23,28 +23,33 @@ def escuchar():
 
 
 # Cargar modelo y tokenizer
-model = tf.keras.models.load_model("liz/liz_0001.keras")
-with open("liz/tokenizer.pickle", "rb") as handle:
-    tokenizer = pickle.load(handle)
+model: keras.Sequential = keras.models.load_model("liz/liz_0000.keras")
+with open("liz/vectorizer.pickle", "rb") as handle:
+    vectorizer = pickle.load(handle)
 with open("liz/label_mapping.pickle", "rb") as handle:
-    label_mapping = pickle.load(handle)
+    label_mapping: dict = pickle.load(handle)
 
 # Funciones para ejecutar comandos
 def open_browser(): os.system("start chrome")
 def close_browser(): os.system("taskkill /IM chrome.exe /F")
 def open_music_player(): os.system("start vlc")
+def close_music_player(): os.system("taskkill /IM vlc.exe /F")
+def open_whatsapp(): os.system("start whatsapp")
+def close_whatsapp(): os.system("taskkill /IM WhatsApp.exe /F")
 
 actions = {
     "open_browser": open_browser,
     "close_browser": close_browser,
-    "open_music_player": open_music_player
+    "open_music_player": open_music_player,
+    "close_music_player": close_music_player,
+    "open_whatsapp": open_whatsapp,
+    "close_whatsapp": close_whatsapp,
 }
 
 # Predicción de intención
 def get_intent(text):
-    sequence = tokenizer.texts_to_sequences([text])
-    padded = tf.keras.preprocessing.sequence.pad_sequences(sequence, maxlen=model.input_shape[1], padding='post')
-    prediction = model.predict(padded)
+    vectorized_input = vectorizer([text])
+    prediction = model.predict(vectorized_input)
     intent = list(label_mapping.keys())[np.argmax(prediction)]
     return intent
 
